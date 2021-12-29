5a8b20191cff54b102afe3500d26e93c93c1e36b
Ali Sokmen <sokmena@gmail.com>
2021-12-29 19:23:34 +0300

    Student object created

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +18,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
+	public List<Student> hello() {
 
-		return List.of("Hello", "World!");
+		return List.of(
+				new Student(1L,
+						"Ali",
+						"ali@ali.com",
+						23,
+						LocalDate.of(2000,12,3)
+
+		));
 	}
 }
