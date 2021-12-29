ca67d6e3a1808d2550bf091c3b05cc38044b42e3
Ali Sokmen <sokmena@gmail.com>
2021-12-29 19:29:11 +0300

    Created API layer for Student

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,32 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello() {
-
-		return List.of(
-				new Student(1L,
-						"Ali",
-						"ali@ali.com",
-						23,
-						LocalDate.of(2000,12,3)
-
-		));
-	}
 }
